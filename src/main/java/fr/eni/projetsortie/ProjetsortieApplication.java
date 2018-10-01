2e4501ec7d3093b807b8d6ae08eabc7feca84047
Kevin <kevin.chapelet2017@campus-eni.fr>
2018-10-01 11:46:45 +0200

    Initial commit

---
+++ src/main/java/fr/eni/projetsortie/ProjetsortieApplication.java
@@ -0,0 +1,12 @@
+package fr.eni.projetsortie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetsortieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetsortieApplication.class, args);
+	}
+}
