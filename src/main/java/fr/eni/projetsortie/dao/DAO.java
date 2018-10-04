4e5556122adfe1ef834a937112d2e16cf5220559
Pierre Boudeaud <pboudeaud@gmail.com>
2018-10-04 10:24:39 +0200

    Add interfaces for DAO and Service
    Add DAO interface with defaults methods
    Add Service methods with defaults methods

---
+++ src/main/java/fr/eni/projetsortie/dao/DAO.java
@@ -0,0 +1,14 @@
+package fr.eni.projetsortie.dao;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface DAO<T> {
+    int save(T entity);
+    T get(Object id);
+    List<T> list();
+    void update(T entity);
+    void delete(Object id);
+}
