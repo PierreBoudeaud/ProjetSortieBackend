1e1b7b35e2873a2485b4a2e34fc2c015fe5188f9
Pierre Boudeaud <pboudeaud@gmail.com>
2018-10-04 17:00:35 +0200

    Fix config method and add filter for angular
    Fix config
    Add cord headers with filter

--- src/main/java/fr/eni/projetsortie/conf/WebConfig.java
+++ src/main/java/fr/eni/projetsortie/conf/WebConfig.java
@@ -2,11 +2,19 @@ package fr.eni.projetsortie.conf;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @ComponentScan(basePackages = { "fr.eni.projetsortie.controller" })
 public class WebConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowCredentials(false)
+                .allowedOrigins("*")
+                .allowedMethods("GET", "PUT", "POST", "DELETE");
 
+    }
 }
