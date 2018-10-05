faf715113373aa8e4321669d4d85d7c4123b2da3
Pierre Boudeaud <pboudeaud@gmail.com>
2018-10-05 16:20:13 +0200

    Add Lieu service

---
+++ src/main/java/fr/eni/projetsortie/service/LieuServiceImp.java
@@ -0,0 +1,51 @@
+package fr.eni.projetsortie.service;
+
+import fr.eni.projetsortie.dao.LieuDAOImp;
+import fr.eni.projetsortie.dao.VilleDAOImp;
+import fr.eni.projetsortie.model.Lieu;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@org.springframework.stereotype.Service
+@Transactional(readOnly = true)
+public class LieuServiceImp implements Service<Lieu>{
+    @Autowired
+    private LieuDAOImp lieuDAO;
+
+    @Autowired
+    private VilleDAOImp villeDAOImp;
+
+    @Transactional
+    @Override
+    public int save(Lieu entity) {
+        return this.lieuDAO.save(entity);
+    }
+
+    @Override
+    public Lieu get(Object id) {
+        return this.lieuDAO.get(id);
+    }
+
+    @Override
+    public List<Lieu> list() {
+        return this.lieuDAO.list();
+    }
+
+    public List<Lieu> listByVille(int idVille) {
+        return this.lieuDAO.listByVille(idVille);
+    }
+
+    @Transactional
+    @Override
+    public void update(Lieu entity) {
+        entity.setVille(this.villeDAOImp.get(entity.getVille().getId()));
+        this.lieuDAO.update(entity);
+    }
+
+    @Override
+    public void delete(Object id) {
+        this.lieuDAO.delete(id);
+    }
+}
